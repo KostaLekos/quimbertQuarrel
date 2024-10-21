4e3fa12d7c78a4b54ac94cf96bb170882ca9f3e0
liam <liamgames505@gmail.com>
2024-10-21 08:41:39 -0700

    fix quimbert initialization

--- QuimbertQuarrel.java
+++ QuimbertQuarrel.java
@@ -870,6 +870,12 @@ public class QuimbertQuarrel {
                         if (points == 0 && makingQuimbert + 1 <= quimbertQuantity) {
                             if (makingQuimbert + 1 <= quimbertQuantity) {
                                 quimberts.add(new Quimbert(a, b, sColor2, d, e, f, g, h));
+                                quimberts.get(makingQuimbert).startingHealth = (randint(3, 5) * quimberts.get(makingQuimbert).looks) / 2;
+                                quimberts.get(makingQuimbert).health = quimberts.get(makingQuimbert).startingHealth;
+                                if (quimberts.get(makingQuimbert).health < 1) {
+                                    quimberts.get(makingQuimbert).health++;
+                                    quimberts.get(makingQuimbert).startingHealth++;
+                                }
                                 a = 0; b = 0; d = 0; e = 0; f = 0; g = ""; h = "";
                                 textBoxName.clear();
                                 textBoxOwner.clear();
